54088d8efa8a3618a0ab58f89e77c2e39e1cab2d
Joe Hansen <joehansen2@gmail.com>
2013-01-26 15:15:46 -0800

    initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilyToDo::Application.config.secret_token = '9135f1d9d2018eb426a81da26f185f3a47c1fea30447875284e40703a22803678b672f5b6b192304e4675e8ef7c7366cc73331b9c9d6963f065ac0dc0e6358eb'
---
+++ test/functional/projects_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class ProjectsControllerTest < ActionController::TestCase
+  setup do
+    @project = projects(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:projects)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create project" do
+    assert_difference('Project.count') do
+      post :create, project: { Cost: @project.Cost, Family_id: @project.Family_id, NeededMaterials: @project.NeededMaterials, TimeRequired: @project.TimeRequired, name: @project.name, status_id: @project.status_id }
+    end
+
+    assert_redirected_to project_path(assigns(:project))
+  end
+
+  test "should show project" do
+    get :show, id: @project
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @project
+    assert_response :success
+  end
+
+  test "should update project" do
+    put :update, id: @project, project: { Cost: @project.Cost, Family_id: @project.Family_id, NeededMaterials: @project.NeededMaterials, TimeRequired: @project.TimeRequired, name: @project.name, status_id: @project.status_id }
+    assert_redirected_to project_path(assigns(:project))
+  end
+
+  test "should destroy project" do
+    assert_difference('Project.count', -1) do
+      delete :destroy, id: @project
+    end
+
+    assert_redirected_to projects_path
+  end
+end
